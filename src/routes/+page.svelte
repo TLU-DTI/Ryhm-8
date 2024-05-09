e2774446ce0ad4bc0fd54e354e55abb5cf226cbf
MRmikimous <germotael@gmail.com>
2024-05-09 15:57:57 +0300

    tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Click</button>
+
+<style lang="scss">
+</style>
