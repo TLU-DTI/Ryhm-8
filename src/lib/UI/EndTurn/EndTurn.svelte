a18cc8040ff23465b677b4771efbec1ea783a98b
Madis V. <146342746+madis789tl@users.noreply.github.com>
2024-06-14 13:49:27 +0300

    test

--- src/lib/UI/EndTurn/EndTurn.svelte
+++ src/lib/UI/EndTurn/EndTurn.svelte
@@ -7,6 +7,55 @@
       EndTurn.EndTurnState.toggle();
     }, 1e3);
   });
+<<<<<<< Updated upstream
+=======
+
+  function test():void{
+
+    Objective.ObjectiveCost.move(MitigateCards.MitigatCardState.getRandomCard().attributes.cost);
+    Objective.ObjectiveQuality.move(MitigateCards.MitigatCardState.getRandomCard().attributes.quality);
+    Objective.ObjectiveScope.move(MitigateCards.MitigatCardState.getRandomCard().attributes.scope);
+    Objective.ObjectiveTime.move(MitigateCards.MitigatCardState.getRandomCard().attributes.time);
+    console.log(Timeline.TimelineState.barPos);
+    if (Timeline.TimelineState.barPos < 18){
+      Timeline.TimelineState.move(28);
+    }
+    else if (Timeline.TimelineState.barPos < 140){
+      Timeline.TimelineState.move(28);
+      stage = 1
+    }
+    else if (Timeline.TimelineState.barPos < 180){
+      Timeline.TimelineState.move(28);
+      stage = 2
+    }
+    else{
+      stage = 3
+    }
+    //Timeline.TimelineState.move(27);
+    console.log(Timeline.TimelineState.barPos);
+  }
+
+  //Esimene mull (initation) algab positsooonil 17
+
+
+
+  function save():void{
+    SaveGameState.SaveGameState().saveGame({
+      "objectives": {
+        "scope": Objective.ObjectiveScope.barPos,
+        "quality": Objective.ObjectiveQuality.barPos,
+        "time": Objective.ObjectiveTime.barPos,
+        "cost": Objective.ObjectiveCost.barPos
+      },
+      "mitigationCards": MitigateCards.MitigatCardState.cards,
+      "riskCards": RiskCards.RiskCardState.riskCards,
+      "manager": Manager.ManagerLogsState.logs,
+      "logs" : RiskLogs.RiskLogsState.logs,
+      "timelineStage": stage,
+      "round": Timeline.TimelineState.barPos
+    })
+  }
+>>>>>>> Stashed changes
 </script>
 
 <button class="flex size-full items-center justify-center">
--- src/lib/UI/Manager/Manager.svelte
+++ src/lib/UI/Manager/Manager.svelte
@@ -2,6 +2,7 @@
   import SimpleBar from '$lib/Components/SimpleBar.svelte';
   import ManagerLogs, { type IManagerLog } from '$lib/States/ManagerLogState.svelte';
   import ManagerLogSvg from './ManagerLogSVG.svelte';
+  import MitigateCards from '$lib/Cards/MitigationCards.svelte';
 
   let logs = $state([]) as IManagerLog[];
 
@@ -11,10 +12,57 @@
     setInterval(() => {
       ManagerLogs.ManagerLogsState.addLog({
         message: 'You are fired!!!',
-        title: 'A messeage from the boss:'
+        title: 'Welcome!',
+        name: 'Joel'
       });
     }, 1e3);
   });
+
+  if (MitigateCards.MitigatCardState.getRandomCard().rng) {
+      console.log('1');
+      if (MitigateCards.MitigatCardState.getRandomCard().id == 'MT07') {
+      console.log('1');
+    }
+    else if  (MitigateCards.MitigatCardState.getRandomCard().id == 'MT08') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'MT09') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'MT11') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'MT13') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'MT14') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'MT15') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'MC08') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'ME02') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'ME03') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'ME04') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'ME06') {
+      console.log('1');
+    }
+    else if (MitigateCards.MitigatCardState.getRandomCard().id == 'ME12') {
+      console.log('1');
+    }
+    }
+
+
+
 </script>
 
 <div class="size-full content-center text-center">
@@ -24,6 +72,7 @@
         {#each logs as log}
           <ul class="bg-white">
             <li>{log.title}</li>
+            <li>{log.name}</li>
             <li>{log.message}</li>
           </ul>
         {/each}
