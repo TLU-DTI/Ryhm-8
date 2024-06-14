ba3b942919aab6f02a5386234563c11c519db303
Madis V. <146342746+madis789tl@users.noreply.github.com>
2024-06-14 10:50:14 +0300

    Manageri kood

--- src/lib/UI/Manager/Manager.svelte
+++ src/lib/UI/Manager/Manager.svelte
@@ -1,3 +1,33 @@
+<script lang="ts">
+  import SimpleBar from '$lib/Components/SimpleBar.svelte';
+  import ManagerLogs, { type IManagerLog } from '$lib/States/ManagerLogState.svelte';
+  import ManagerLogSvg from './ManagerLogSVG.svelte';
+
+  let logs = $state([]) as IManagerLog[];
+
+  $effect(() => {
+    logs = ManagerLogs.ManagerLogsState.logs;
+    console.log ('tootab')
+    setInterval(() => {
+      ManagerLogs.ManagerLogsState.addLog({
+        message: 'You are fired!!!',
+        title: 'A messeage from the boss:'
+      });
+    }, 1e3);
+  });
+</script>
+
 <div class="size-full content-center text-center">
-  <p>Manager</p>
+  <ManagerLogSvg>
+    <SimpleBar>
+      <div class="bg-black">
+        {#each logs as log}
+          <ul class="bg-white">
+            <li>{log.title}</li>
+            <li>{log.message}</li>
+          </ul>
+        {/each}
+      </div>
+    </SimpleBar>
+  </ManagerLogSvg>
 </div>
