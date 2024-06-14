14c89e126e0308e88cec9511867cdc32c2eb14aa
Jan-Erich <146342622+JanErichS@users.noreply.github.com>
2024-06-14 15:01:55 +0300

    save

--- src/lib/UI/CardHand/CardHand.svelte
+++ src/lib/UI/CardHand/CardHand.svelte
@@ -2,7 +2,45 @@
   import CardTest from '../ActionCard/CardTest.svelte';
 </script>
 
-<div class="size-full content-center text-center">
-  <CardTest />
-  <p>Card Hand</p>
+<div class="mb-3 ml-3 mr-3 mt-3 flex size-full content-center text-center">
+  <div class="card-container">
+    <div class="card"><CardTest /></div>
+  </div>
+  <div class="card-container">
+    <div class="card"><CardTest /></div>
+  </div>
+  <div class="card-container">
+    <div class="card"><CardTest /></div>
+  </div>
+  <div class="card-container">
+    <div class="card"><CardTest /></div>
+  </div>
+  <div class="card-container">
+    <div class="card"><CardTest /></div>
+  </div>
+  <!-- <p>Card Hand</p> -->
 </div>
+
+<style>
+  .card-container {
+    position: relative;
+    flex: 1 1 auto;
+    max-width: inherit;
+    width: inherit;
+    height: inherit;
+  }
+  .card {
+    position: absolute;
+    top: 0;
+    left: 0;
+    box-sizing: border-box;
+    height: 100%;
+  }
+  .card-container:last-child {
+    flex: 1 0 50%;
+  }
+  .card:hover {
+    z-index: 1;
+    cursor: pointer;
+  }
+</style>
