f7b0599b8297d245f77c456f5a060c17d5b3fed8
Jan-Erich <146342622+JanErichS@users.noreply.github.com>
2024-06-17 15:45:18 +0300

    save

--- src/lib/UI/CardHand/CardHand.svelte
+++ src/lib/UI/CardHand/CardHand.svelte
@@ -1,49 +1,80 @@
-<script>
+<script lang="ts">
+  import { Dom } from '@svgdotjs/svg.js';
   import CardTest from '../ActionCard/CardTest.svelte';
+  import anime from 'animejs';
+
+  let move;
+
+  $effect(() => {
+    move = function (targetID: string) {
+      const finalPos = document.getElementById('RiskLogs');
+      console.log(finalPos?.clientTop);
+
+      const animation1 = anime.timeline().add({
+        targets: '#',
+        //rotate: 7200,
+        duration: 5000,
+        translateY: -2000,
+        easing: 'easeInOutSine',
+        direction: 'normal',
+        loop: true
+      });
+      const animation2 = anime.timeline().add({
+        targets: '#' + targetID,
+        duration: 500,
+        top: finalPos,
+        left: 0,
+        scale: 0.3,
+        rotate: '1turn'
+      });
+    };
+  });
 </script>
 
 <div class="flex size-full content-center justify-center text-center">
   <div class="card-container">
-    <div class="card">
+    <button class="card" id="card1" onclick={() => move('card1')}>
       <CardTest
-        title="TIIIITEL"
+        title="TIIIITEL 🤷‍♀️"
         description="Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description "
       />
-    </div>
+    </button>
   </div>
   <div class="card-container">
-    <div class="card">
+    <button class="card" id="card2" onclick={() => move('card2')}>
       <CardTest
         title="TIIIITEL"
         description="Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description "
       />
-    </div>
-  </div>
-  <div class="card-container">
-    <div class="card">
-      <CardTest
-        title="TIIIITEL"
-        description="Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description "
-      />
-    </div>
-  </div>
-  <div class="card-container">
-    <div class="card">
-      <CardTest
-        title="TIIIITEL"
-        description="Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description "
-      />
-    </div>
-  </div>
-  <div class="card-container">
-    <div class="card">
-      <CardTest
-        title="TIIIITEL"
-        description="Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description "
-      />
-    </div>
+    </button>
   </div>
 </div>
+
+<!-- <div class="card-container">
+    <div class="card">
+      <CardTest
+        title="TIIIITEL"
+        description="Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description "
+      />
+    </div>
+  </div>
+  <div class="card-container">
+    <div class="card">
+      <CardTest
+        title="TIIIITEL"
+        description="Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description "
+      />
+    </div>
+  </div>
+  <div class="card-container">
+    <div class="card">
+      <CardTest
+        title="TIIIITEL"
+        description="Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description "
+      />
+    </div>
+  </div> 
+</div> -->
 
 <style>
   .card-container {
