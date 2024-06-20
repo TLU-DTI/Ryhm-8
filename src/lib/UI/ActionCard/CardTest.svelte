3648fad4820a133826f82357183069f81e315f2e
Hugo Luca Tigane <146342693+HugoLucaT@users.noreply.github.com>
2024-06-20 14:11:47 +0300

    bla

---
+++ src/lib/UI/ActionCard/CardTest.svelte
@@ -0,0 +1,193 @@
+<script lang="ts">
+  let { title, description , img}: { title: string; description: string ; img: string} = $props();
+  
+</script>
+
+<svg
+  id="Layer_2"
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  viewBox="0 0 183.71 255.68"
+  style="width: auto; height: inherit;"
+>
+  <defs
+    ><linearGradient
+      id="linear-gradient"
+      x1="1.2"
+      y1="130.18"
+      x2="175.63"
+      y2="130.18"
+      gradientTransform="translate(0 258.02) scale(1 -1)"
+      gradientUnits="userSpaceOnUse"
+      ><stop offset="0" stop-color="#43466e" /><stop
+        offset="1"
+        stop-color="#585e81"
+      /></linearGradient
+    ><linearGradient
+      id="linear-gradient-2"
+      x1="32.57"
+      y1="248.29"
+      x2="41.35"
+      y2="248.29"
+      gradientTransform="translate(0 258.02) scale(1 -1)"
+      gradientUnits="userSpaceOnUse"
+      ><stop offset="0" stop-color="#fa6b49" /><stop
+        offset="1"
+        stop-color="#feb749"
+      /></linearGradient
+    ><linearGradient
+      id="linear-gradient-3"
+      x1="44.63"
+      y1="248.29"
+      x2="53.42"
+      y2="248.29"
+      gradientTransform="translate(0 258.02) scale(1 -1)"
+      gradientUnits="userSpaceOnUse"
+      ><stop offset="0" stop-color="#fa6b49" /><stop
+        offset="1"
+        stop-color="#feb749"
+      /></linearGradient
+    ><linearGradient
+      id="linear-gradient-4"
+      x1="123.41"
+      y1="248.29"
+      x2="132.19"
+      y2="248.29"
+      gradientTransform="translate(0 258.02) scale(1 -1)"
+      gradientUnits="userSpaceOnUse"
+      ><stop offset="0" stop-color="#fa6b49" /><stop
+        offset="1"
+        stop-color="#feb749"
+      /></linearGradient
+    ><linearGradient
+      id="linear-gradient-5"
+      x1="135.47"
+      y1="248.29"
+      x2="144.26"
+      y2="248.29"
+      gradientTransform="translate(0 258.02) scale(1 -1)"
+      gradientUnits="userSpaceOnUse"
+      ><stop offset="0" stop-color="#fa6b49" /><stop
+        offset="1"
+        stop-color="#feb749"
+      /></linearGradient
+    ><clipPath id="clippath"
+      ><path
+        d="M28.42,25.63h120.31c6.12,0,11.1,4.97,11.1,11.1v87.96c0,6.26-5.08,11.34-11.34,11.34H28.84c-6.36,0-11.53-5.17-11.53-11.53V36.74c0-6.13,4.98-11.11,11.11-11.11Z"
+        style="fill:none; stroke-width:0px;"
+      /></clipPath
+    ></defs
+  ><g id="Layer_4"
+    ><rect
+      x="1.2"
+      y="1.2"
+      width="174.43"
+      height="253.28"
+      rx="14.83"
+      ry="14.83"
+      style="fill:url(#linear-gradient); stroke:#6e78b3; stroke-miterlimit:10; stroke-width:2.4px;"
+    /><rect
+      x="7.35"
+      y="16.02"
+      width="162.12"
+      height="232.31"
+      rx="10.4"
+      ry="10.4"
+      style="fill:#40406b; stroke-width:0px;"
+    /><rect
+      x="8.89"
+      y="17.56"
+      width="159.04"
+      height="229.23"
+      rx="8.4"
+      ry="8.4"
+      style="fill:#282844; stroke-width:0px;"
+    /><path
+      d="M150.99,12.49h-1.35c-.39,0-.65-.38-.52-.75l2.13-5.89c.08-.22.29-.37.52-.37h1.35c.39,0,.65.38.52.75l-2.13,5.89c-.08.22-.29.37-.52.37Z"
+      style="fill:#282844; stroke-width:0px;"
+    /><path
+      d="M155.38,12.49h-1.35c-.39,0-.65-.38-.52-.75l2.13-5.89c.08-.22.29-.37.52-.37h1.35c.39,0,.65.38.52.75l-2.13,5.89c-.08.22-.29.37-.52.37Z"
+      style="fill:#282844; stroke-width:0px;"
+    /><path
+      d="M159.77,12.49h-1.35c-.39,0-.65-.38-.52-.75l2.13-5.89c.08-.22.29-.37.52-.37h1.35c.39,0,.65.38.52.75l-2.13,5.89c-.08.22-.29.37-.52.37Z"
+      style="fill:#282844; stroke-width:0px;"
+    /><path
+      d="M25.84,12.49h1.35c.39,0,.65-.38.52-.75l-2.13-5.89c-.08-.22-.29-.37-.52-.37h-1.35c-.39,0-.65.38-.52.75l2.13,5.89c.08.22.29.37.52.37Z"
+      style="fill:#282844; stroke-width:0px;"
+    /><path
+      d="M21.44,12.49h1.35c.39,0,.65-.38.52-.75l-2.13-5.89c-.08-.22-.29-.37-.52-.37h-1.35c-.39,0-.65.38-.52.75l2.13,5.89c.08.22.29.37.52.37Z"
+      style="fill:#282844; stroke-width:0px;"
+    /><path
+      d="M17.05,12.49h1.35c.39,0,.65-.38.52-.75l-2.13-5.89c-.08-.22-.29-.37-.52-.37h-1.35c-.39,0-.65.38-.52.75l2.13,5.89c.08.22.29.37.52.37Z"
+      style="fill:#282844; stroke-width:0px;"
+    /><path
+      d="M168.14,11.33c0,.4-.33.73-.73.73s-.73-.33-.73-.73.33-.73.73-.73.73.33.73.73Z"
+      style="fill:#282844; stroke-width:0px;"
+    /><path
+      d="M168.01,11.33c0,.34-.27.61-.61.61s-.61-.27-.61-.61.27-.61.61-.61.61.27.61.61Z"
+      style="fill:#28bbff; stroke-width:0px;"
+    /><path
+      d="M10.15,11.33c0,.4-.33.73-.73.73s-.73-.33-.73-.73.33-.73.73-.73.73.33.73.73Z"
+      style="fill:#282844; stroke-width:0px;"
+    /><path
+      d="M10.03,11.33c0,.34-.27.61-.61.61s-.61-.27-.61-.61.27-.61.61-.61.61.27.61.61Z"
+      style="fill:#28bbff; stroke-width:0px;"
+    /><path
+      d="M33.49,7.8c1.5-.19,4.54-.47,6.98-.06.51.09.88.53.88,1.04v2.01c0,.57-.44,1.03-1.01,1.05-1.55.06-4.57.15-6.79.03-.56-.03-.99-.49-.99-1.05v-1.98c0-.53.4-.98.93-1.05h0Z"
+      style="fill:url(#linear-gradient-2); stroke-width:0px;"
+    /><path
+      d="M45.56,7.8c1.5-.19,4.54-.47,6.98-.06.51.09.88.53.88,1.04v2.01c0,.57-.44,1.03-1.01,1.05-1.55.06-4.57.15-6.79.03-.56-.03-.99-.49-.99-1.05v-1.98c0-.53.4-.98.93-1.05h0Z"
+      style="fill:url(#linear-gradient-3); stroke-width:0px;"
+    /><path
+      d="M124.33,7.8c1.5-.19,4.54-.47,6.98-.06.51.09.88.53.88,1.04v2.01c0,.57-.44,1.03-1.01,1.05-1.55.06-4.57.15-6.79.03-.56-.03-.99-.49-.99-1.05v-1.98c0-.53.4-.98.93-1.05h0Z"
+      style="fill:url(#linear-gradient-4); stroke-width:0px;"
+    /><path
+      d="M136.39,7.8c1.5-.19,4.54-.47,6.98-.06.51.09.88.53.88,1.04v2.01c0,.57-.44,1.03-1.01,1.05-1.55.06-4.57.15-6.79.03-.56-.03-.99-.49-.99-1.05v-1.98c0-.53.4-.98.93-1.05h0Z"
+      style="fill:url(#linear-gradient-5); stroke-width:0px;"
+    /><rect
+      x="16.66"
+      y="25.09"
+      width="143.51"
+      height="111.45"
+      rx="10.73"
+      ry="10.73"
+      style="fill:none; stroke:#6e78b3; stroke-miterlimit:10; stroke-width:1.78px;"
+    /><g style="isolation:isolate;"
+      ><text
+        transform="translate(65.43 12.64)"
+        style="fill:#ff9d00; font-family:ChangaOne, &apos;Changa One&apos;; font-size:9.93px; isolation:isolate;"
+        ><tspan x="0" y="0">Risk Card</tspan></text
+      ></g
+    ><g style="opacity:.86;"
+      ><g style="isolation:isolate;">
+        <foreignObject
+          x="10"
+          y="140"
+          width="156"
+          height="20"
+          style="color:#f3edf7; font-family:ChangaOne, &apos;Changa One&apos;; font-size:13px; isolation:isolate; overflow:hidden;"
+        >
+          {title}
+        </foreignObject>
+        <foreignObject
+          x="10"
+          y="160"
+          width="156"
+          height="82"
+          style="color:#f3edf7; font-family:Lato-Regular, Lato; font-size:10px; isolation:isolate;"
+        >
+          {description}
+        </foreignObject>
+      </g></g
+    >
+    <g style="clip-path:url(#clippath);"
+      ><image
+        width="1025"
+        height="1025"
+        transform="translate(12.79 18.73) scale(.15)"
+        xlink:href = "/assets/Pictures/RiskCards/{img}.jpg"
+        
+      /></g
+    ></g
+  >
+</svg>
