4e3c47e163582bba5d6a0b01e83864143f116467
MRmikimous <germotael@gmail.com>
2024-06-20 22:16:39 +0300

    save

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,51 +1,9 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
-  import RiskCards from '$lib/Cards/RiskCards.svelte';
-  import MitigateCards from '$lib/Cards/MitigationCards.svelte';
-  import RiskLogs from '$lib/States/RiskLogState.svelte';
-  import EndTurn from '$lib/States/EndTurnState.svelte';
-  import Objective from '$lib/States/ObjectiveState.svelte';
-  import Timeline from '$lib/States/TimelineState.svelte';
   import '../app.css';
   let { children }: { children: Snippet } = $props();
 
-  $effect.pre(() => {
-    RiskLogs.RiskLogsState = RiskLogs.RiskLogs();
-    RiskCards.RiskCardState = RiskCards.Cards();
-    MitigateCards.MitigatCardState = MitigateCards.Cards();
-    Objective.ObjectiveCost = Objective.Objective(
-      '#Objectives_Cost-progress-bar',
-      '#Objectives_Cost-progress-track'
-    );
-
-    Objective.ObjectiveQuality = Objective.Objective(
-      '#Objectives_Quality-progress-bar',
-      '#Objectives_Quality-progress-track'
-    );
-
-    Objective.ObjectiveScope = Objective.Objective(
-      '#Objectives_Scope-progress-bar',
-      '#Objectives_Scope-progress-track'
-    );
-
-    Objective.ObjectiveTime = Objective.Objective(
-      '#Objectives_Time-progress-bar',
-      '#Objectives_Time-progress-track'
-    );
-
-    Timeline.TimelineState = Timeline.Timeline(
-      '#Timeline_progress-bar',
-      '#Timeline_progress-bar-track'
-    );
-
-    EndTurn.EndTurnState = EndTurn.EndTurn({
-      strip: '#EndTurn_strip',
-      background: '#EndTurn_background',
-      border: '#EndTurn_border',
-      shadow: '#EndTurn_shadow',
-      button: 'EndTurn_button'
-    });
-  });
+  import '../app.css';
 </script>
 
 {@render children()}
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  import CardHand from '$lib/UI/CardHand/CardHand.svelte';
+  import EndTurn from '$lib/UI/EndTurn/EndTurn.svelte';
+  // import MenuRestart from '$lib/UI/Menu-Restart/Menu-Restart.svelte';
+  import MitigationCards from '$lib/UI/MitigationCards/MitigationCards.svelte';
+  import Manager from '$lib/UI/Notification/Notification.svelte';
+  import Objective from '$lib/UI/Objective/Objective.svelte';
+  import RiskLog from '$lib/UI/RiskLog/RiskLogs.svelte';
+  import Timeline from '$lib/UI/Timeline/Timeline.svelte';
+</script>
+
+<div class="wrapper">
+  <div class="flex">
+    <!-- Column 1 -->
+    <div class="w-[20vw]">
+      <div class="relative h-[50vh]">
+        <Objective />
+      </div>
+
+      <div class="h-[4vh]"></div>
+
+      <div class="relative h-[46vh]">
+        <RiskLog />
+      </div>
+    </div>
+
+    <!-- Divider -->
+    <div class="w-[6vw]">
+      <div class="h-[100vh]"></div>
+    </div>
+
+    <!-- Column 2 -->
+    <div class="w-[48vw]">
+      <div class="relative h-[10vh]">
+        <Timeline />
+      </div>
+
+      <div class="h-[1vh]"></div>
+
+      <div class="relative h-[44vh]">
+        <CardHand />
+      </div>
+
+      <div class="h-[4vh]"></div>
+
+      <div class="relative h-[41vh]">
+        <MitigationCards />
+      </div>
+    </div>
+
+    <!-- Divider -->
+    <div class="w-[1vw]">
+      <div class="h-[100vh]"></div>
+    </div>
+
+    <!-- Column 3 -->
+    <div class="w-[25vw]">
+      <div class="relative h-[10vh]">
+        <!-- <MenuRestart /> -->
+      </div>
+
+      <div class="h-[6vh]"></div>
+
+      <div class="relative h-[24vh]">
+        <Manager />
+      </div>
+
+      <div class="h-[42vh]"></div>
+      <div class="h-[2vh]"></div>
+
+      <div class="relative flex h-[7vh]">
+        <div class="w-[8vw]"></div>
+        <EndTurn />
+        <div class="w-[8vw]"></div>
+      </div>
+
+      <div class="h-[6vh]"></div>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .wrapper {
+    background-repeat: no-repeat;
+    background-image: url('/assets/bg/filter.svg'), url('/assets/bg/bg.jpg');
+    background-size: cover;
+  }
+</style>
