2883e89888d9e3bf39455ae6acdf413c5db355bd
Germo Tael <germotael@gmail.com>
2024-08-23 02:06:44 +0300

    it's rewind time

--- src/lib/UI/CardHand/CardHand_New.svelte
+++ src/lib/UI/CardHand/CardHand_New.svelte
@@ -9,7 +9,7 @@
   }, 1e3);
 </script>
 
-<div class="handbg mb-3 ml-3 mr-3 mt-3 flex size-full content-center text-center">
+<div id="card-hand" class="handbg absolute flex size-full pb-4 pl-4 pr-4 pt-4">
   {#each cards as card}
     <div class="card-container">
       <div class="hitbox"></div>
@@ -29,21 +29,24 @@
   }
 
   .card-container {
-    cursor: default !important;
     display: flex;
     justify-content: center;
     align-items: center;
     position: relative;
-    flex: 1 1 auto;
-    flex-wrap: wrap;
+    /* flex: 1 1 auto;
+    flex-wrap: wrap; */
     max-width: inherit;
     width: inherit;
     height: inherit;
     transition: transform 0.3s ease-in-out;
   }
+
+  .card-container:not(:first-child) {
+    margin-left: -70px;
+  }
+
   .card,
   .hitbox {
-    cursor: default !important;
     position: absolute;
     top: 0;
     left: 0;
@@ -53,10 +56,10 @@
     height: 100%;
     flex-basis: calc(33.33% - 1rem);
     transition:
-      transform 0.3s ease-in-out,
-      box-shadow 0.3s ease-in-out,
-      border-width 0.3s ease-in-out,
-      rotate 0.3s ease-in-out;
+      transform 0.2s ease-in-out,
+      box-shadow 0.2s ease-in-out,
+      border-width 0.2s ease-in-out,
+      rotate 0.2s ease-in-out;
   }
 
   .hitbox {
@@ -64,15 +67,16 @@
     width: 100%;
   }
 
-  .card-container:last-child {
+  /* .card-container:last-child {
     flex: 1 0 50%;
-  }
+  } */
   .hitbox:hover + .card {
     z-index: 2;
     cursor: pointer;
-    transform: scale(1.05);
+    transform: scale(1.2);
     rotate: 3deg;
   }
+
   .card-container:nth-child(even):hover .card {
     rotate: -3deg;
   }
