360ed71aa3a683cbe659cc2b9b48d70859a1069c
MRmikimous <germotael@gmail.com>
2024-10-15 22:27:14 +0300

    fix

--- src/lib/Engine/Cards/index.ts
+++ src/lib/Engine/Cards/index.ts
@@ -1,7 +1,5 @@
 import { mount, unmount } from "svelte";
 import type { Engine } from "../engine.svelte";
-import type { MitiCard } from "./MitiCard.svelte";
-import type { RiskCard } from "./RiskCard.svelte";
 import RiskAnimation from "$lib/Components/RiskAnimation.svelte";
 import { RiskLog } from "../risklog.svelte";
 import { Notification } from '../notification.svelte';
@@ -21,6 +19,49 @@ export function handleCardOnCard(engine: Engine, riskID: string, mitiID?: string
   let time = 0;
   let cost = 0;
 
+  if (ignored) {
+    notification.add(new Notification("Manager", `You decided to ignore the following risk: <b>${riskCard.title}</b>`, "Happy"));
+
+    scope = riskCard.attributes.scope;
+    quality = riskCard.attributes.quality;
+    time = riskCard.attributes.time;
+    cost = riskCard.attributes.cost;
+
+    risklog.add(
+      new RiskLog(riskCard.title, riskCard.category, "Ignored", {
+        scope: scope,
+        quality: quality,
+        time: time,
+        cost: cost
+      })
+    );
+
+    objective.scope = scope;
+    objective.quality = quality;
+    objective.time = time;
+    objective.cost = cost;
+
+    riskhand.handCards = riskhand.handCards.filter(card => card.id !== riskCard.id);
+
+    riskCard.timeout = engine.riskhand.RISKCARD_TIMEOUT_ROUNDS;
+    riskhand.usedCards.push(riskCard);
+
+    const domCard = document.getElementById("RiskCard_" + riskCard.id)!.getBoundingClientRect()
+
+    const a = mount(RiskAnimation, {
+      target: document.getElementById("riskanimation")!,
+      props: { riskCard, current: domCard }
+    });
+
+    // race condition go brr
+    setTimeout(() => {
+      unmount(a);
+    }, 550);
+
+    return;
+  }
+
+
   // if miticard beats riskcard
   let correct = false;
   riskCard.mitigation?.forEach(id => {
@@ -29,14 +70,7 @@ export function handleCardOnCard(engine: Engine, riskID: string, mitiID?: string
     }
   })
 
-  if (ignored) {
-    notification.add(new Notification("Manager", `You decided to ignore the following risk: <b>${riskCard.title}</b>`, "Happy"));
-
-    scope = riskCard.attributes.scope;
-    quality = riskCard.attributes.quality;
-    time = riskCard.attributes.time;
-    cost = riskCard.attributes.cost;
-  } else if (correct) {
+  if (correct) {
     notification.add({
       name: 'Manager',
       message: `Your choice to use the card <b>${mitiCard.title}</b> on the <b>${riskCard.title}</b> was perfect!`,
@@ -85,7 +119,7 @@ export function handleCardOnCard(engine: Engine, riskID: string, mitiID?: string
   }
 
   risklog.add(
-    new RiskLog(riskCard.title, riskCard.category, ignored ? "Ignored" : mitiCard.title, {
+    new RiskLog(riskCard.title, riskCard.category, mitiCard.title, {
       scope: scope,
       quality: quality,
       time: time,
@@ -98,28 +132,23 @@ export function handleCardOnCard(engine: Engine, riskID: string, mitiID?: string
   objective.time = time;
   objective.cost = cost;
 
-  if (mitihand.usedCards.filter(card => card.id === mitiCard?.id).length == 0 && ignored === false) {
+  if (mitihand.usedCards.filter(card => card.id === mitiCard?.id).length == 0) {
     mitihand.addUsed(mitiCard);
   }
 
   riskhand.handCards = riskhand.handCards.filter(card => card.id !== riskCard.id);
-
-  if (ignored === false) {
-    mitihand.handCards = mitihand.handCards.filter(card => card.id !== mitiCard.id);
-  }
+  mitihand.handCards = mitihand.handCards.filter(card => card.id !== mitiCard.id);
 
   riskCard.timeout = engine.riskhand.RISKCARD_TIMEOUT_ROUNDS;
   riskhand.usedCards.push(riskCard);
 
-  if (ignored === false) {
-    const a = mount(RiskAnimation, {
-      target: document.getElementById("riskanimation")!,
-      props: { riskCard, current: engine.drag.mousePos }
-    });
+  const a = mount(RiskAnimation, {
+    target: document.getElementById("riskanimation")!,
+    props: { riskCard, current: engine.drag.mousePos }
+  });
 
-    // race condition go brr
-    setTimeout(() => {
-      unmount(a);
-    }, 550);
-  }
+  // race condition go brr
+  setTimeout(() => {
+    unmount(a);
+  }, 550);
 }
